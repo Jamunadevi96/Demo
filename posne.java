c89d1ffe64eab33bca39daa96cdff82c34dba083
Jamunadevi96 <jamunadevid@gmail.com>
2017-06-13 10:56:01 +0530

    Create posne.java

---
+++ posne.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import.java.util.*;
+class posne
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner (String args[]);
+n=sc.next();
+if(n>0)
+System.out.println("println("positive number");
+else if(n<0)
+System.out.println("negative number");
+else(n=0)
+System.out.println("zero");
+}
+}
+}

8c2c9075d337a7c191f5e69ee29792c0febb747c
Jamunadevi96 <jamunadevid@gmail.com>
2017-06-14 12:21:26 +0530

    Create largestno.java

---
+++ largestno.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class largestno
+{
+public static void main(string args[])
+{
+int a[]=new int[3];
+Scanner sc=new Scanner(System.in);
+a[i]=sc.next();
+for(i=0;i<arr.length;i++)
+{

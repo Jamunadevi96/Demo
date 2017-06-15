0f76c8594caaae6883150dc95b397bd2684f1d0a
Jamunadevi96 <jamunadevid@gmail.com>
2017-06-15 10:34:47 +0530

    Create year.java

--- year.java
+++ year.java
@@ -7,14 +7,13 @@ public static void main(String args[])
 int n;
 Scanner sc=new Scanner(System.in);
 n=sc.nextInt();
-while(n>0)
 {
 if(n%4==0)
 {
-System.out.println("Leap year");
+System.out.println(n+" Leap year");
 }
 else
-Sysstem.out.println("Not a Leap year");
+System.out.println(n+" Not a Leap year");
 }
 }
 }

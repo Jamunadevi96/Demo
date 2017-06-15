6e4435b01b57a04852ef2aa0e472da47e5cf0ee2
Jamunadevi96 <jamunadevid@gmail.com>
2017-06-15 11:27:39 +0530

    Create naturalno.java

---
+++ naturalno.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class naturalno
+{
+public static void main(String args[])
+{
+int n,sum=0,i;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(i=0;i<n;i++)
+{
+sum=sum+i;
+System.out.println(sum);
+}
+}
+}

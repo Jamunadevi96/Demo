faf197585fa13d4f8c58b83c17d8cda6e02a618b
Jamunadevi96 <jamunadevid@gmail.com>
2017-06-16 10:12:59 +0530

    Update largestno.java

--- largestno.java
+++ largestno.java
@@ -4,14 +4,15 @@ class largestno
 {
 public static void main(string args[])
 {
-int n;
+int n,larg=a[0];
+int a[]=new int[n];
 Scanner sc=new Scanner(System.in);
 int n=sc.next();  
-for(i=0;i<n;i++)
+for(i=0;i<a.length;i++)
 {
-  a[i]=sc.nextInt();
+  a[i]=sc.next();
   if(num>large)
-    num=large;
+  num=large;
 }
   System.out.println(num);
 }

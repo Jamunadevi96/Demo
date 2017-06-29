198bae2e250e3874926af98a6e8906cfb6e0c233
Jamunadevi96 <jamunadevid@gmail.com>
2017-06-29 13:59:43 +0530

    Update primeno.java

--- primeno.java
+++ primeno.java
@@ -4,15 +4,21 @@ class primeno
 {
 public static void main(String args[])
 {
-int n,i;
+int n,i,count=0;
 Scanner sc=new Scanner(System.in);
 n=sc.nextInt();
 for(i=2;i<n;i++)
 {
 if(n%i==0)
+{
+  count++;
+  break;
+}
+}
+ if(count==0)
 System.out.println("Primeno");
 else
 System.out.println("Not a prime no");
 }
 }
-}
+

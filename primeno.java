7c00bc98416059f5fd94fa62b1b804efeb6dccef
Jamunadevi96 <jamunadevid@gmail.com>
2017-06-29 12:28:03 +0530

    Create primeno.java

---
+++ primeno.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class primeno
+{
+public static void main(String args[])
+{
+int n;
+Scanner=new Scanner(System.in);
+n=sc.nextInt();
+for(i=2;i<n;i++)
+{
+if(n%i==0)
+System.out.println("Primeno");
+else
+System.out.println("Not a prime no");
+}
+}
+}

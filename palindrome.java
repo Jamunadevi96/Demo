435be2b46f21309a929b692e2c6491b20f98e193
Jamunadevi96 <jamunadevid@gmail.com>
2017-06-29 11:51:55 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class palindrome
+{
+public static void main(String args[])
+{
+int n,t,rev=0;
+t=n;
+Scanner sc=new Scanner(System.in);
+n=sc.next();
+while(t>0)
+{
+rev=rev*10+(t%10);
+t=t/10;
+}
+if(n==rev)
+System.out.println("palindrome number");
+else
+System.out.println("not a palindrome number");
+}
+}
+
+

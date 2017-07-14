5f078228e969b968520baf669340d8759fb08bf4
Jamunadevi96 <jamunadevid@gmail.com>
2017-07-14 15:49:03 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class fibonacci
+{
+public static void main(String args[])
+{
+int n1,n2,n3=0;
+for(int i=0;i<n;i++)
+{
+n3=n1+n2;
+n1=n2;
+n2=n3
+}
+System.out.println(n3);
+}
+}

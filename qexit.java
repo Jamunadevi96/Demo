537562cdbf2cdaf4024c752076a8e0afbb3bce2a
Jamunadevi96 <jamunadevid@gmail.com>
2017-07-24 18:21:36 +0530

    Create qexit.java

---
+++ qexit.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class qexit
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s="Q";
+if(s=='Q')
+{
+exit(0);
+}
+}
+}

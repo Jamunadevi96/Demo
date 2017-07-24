2a381e196f3dd861f4cc553388d3a91cb774a64e
Jamunadevi96 <jamunadevid@gmail.com>
2017-07-24 17:46:11 +0530

    Create fib.java

---
+++ fib.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class fib
+{
+public static void main(String args[])
+{
+int sum,a=0,b=1;
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=1;i<=n;++i)
+{
+
+System.out.println(sum+" ");
+sum=a+b;
+a=b;
+b=sum;
+}
+}
+}

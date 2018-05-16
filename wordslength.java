a6ce621d997cbd312c943c0805e62e74b5a331f5
Jamunadevi96 <jamunadevid@gmail.com>
2018-05-16 20:03:09 +0530

    Create wordslength.java

---
+++ wordslength.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class wordslength
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+String[] words=s.split(" ");
+System.out.print(words.length);
+}
+}

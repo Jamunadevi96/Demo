111dfbf92b330085a20edf8aa0bbcea410ab913f
Jamunadevi96 <jamunadevid@gmail.com>
2018-06-15 17:32:01 +0530

    Create holiday.java

---
+++ holiday.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class holiday
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+if(s=="Saturday"||s=="Sunday")
+{
+System.out.print("Yes");
+}
+else
+{
+System.out.print("No");
+}

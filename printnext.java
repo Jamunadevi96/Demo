04d2da16caa2ad9ddf1491deec72ff79ce628151
Jamunadevi96 <jamunadevid@gmail.com>
2018-06-18 17:56:18 +0530

    Create printnext.java

---
+++ printnext.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class printnext
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+System.out.print(n+1);
+}
+}

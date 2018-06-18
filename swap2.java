530298a38956a35661bd978f2578600e1771e481
Jamunadevi96 <jamunadevid@gmail.com>
2018-06-18 16:43:21 +0530

    Create swap2.java

---
+++ swap2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class swap2
+{
+public static void main(String args[])
+{
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+c=a;
+a=b;
+b=c;
+System.out.print(a+" "+b);
+}
+}

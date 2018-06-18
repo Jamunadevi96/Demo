f637650eb978c46f039f23bdadefda08ed1eeab5
Jamunadevi96 <jamunadevid@gmail.com>
2018-06-18 18:18:13 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class concat
+{
+public static void main(String args[])
+{
+Sacnner sc=new Scanner(System.in);
+String s=sc.nextLine();
+String s1=s.replaceAll("\\s","");
+System.out.print(s1);
+}
+}

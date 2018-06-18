62880c242e0a1343d16fa35af17fcc4d1115bca6
Jamunadevi96 <jamunadevid@gmail.com>
2018-06-18 16:52:01 +0530

    Create printno.java

---
+++ printno.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class printno
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+int n=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+System.out.println(s);
+}
+}
+}

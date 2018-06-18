7f851de8e34482c769eb4d07a2e56b7e64cac9e4
Jamunadevi96 <jamunadevid@gmail.com>
2018-06-18 20:07:22 +0530

    Create neareven.java

---
+++ neareven.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class neareven
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n%2==0)
+System.out.print(n);
+else
+System.out.print(n-1);
+}
+}

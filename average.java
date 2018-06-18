279dc7cbf0978304b713a0c29dfcfe5f933a2313
Jamunadevi96 <jamunadevid@gmail.com>
2018-06-18 17:14:26 +0530

    Create average.java

---
+++ average.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class average
+{
+public static void main(String args[])
+{
+int sum=0,avg=0;
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int arr[]=new int[n];
+for(int i=0;i<n;i++)
+{
+arr[]=sc.nextInt();
+sum=sum+a[i];
+}
+avg=sum/n;
+System.out.print(avg);
+}
+}

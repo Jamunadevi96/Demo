028e1f8287fb783edbb82c9a140d9e342fdd1808
Jamunadevi96 <jamunadevid@gmail.com>
2018-06-19 12:39:50 +0530

    Create substring.java

---
+++ substring.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class substring 
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+int n=sc.nextInt();
+System.out.print(s.substring(0,n));
+}
+}

64b4b7ee7ce2f9b8544e6f0dd72767bb69096aff
Jamunadevi96 <jamunadevid@gmail.com>
2018-08-06 09:15:21 +0530

    Create asciiconvert.java

---
+++ asciiconvert.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class asciiconvert
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char ch=sc.next().CharAt(0);
+int ascii=(int)ch;
+System.out.println(ascii);
+}
+}

25cb8841c1f825c1599defe0e9d20743b5dc2d69
Jamunadevi96 <jamunadevid@gmail.com>
2018-08-10 13:49:35 +0530

    Create si.java

---
+++ si.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class si
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int p=sc.nextInt();
+int n=sc.nextInt();
+int r=sc.nextInt();
+System.out.println(p*n*r/100);
+}
+}
